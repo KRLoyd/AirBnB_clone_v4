4b4057de926a5061cbd604fbe58cb97d5a7f9c4c
Kristen Loyd <loydkristen@gmail.com>
2017-09-29 17:44:12 +0000

    Add variable to render_template
    - cache_id = uuid.uuid4()
    - import uuid

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,7 +4,7 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-
+import uuid
 
 # flask setup
 app = Flask(__name__)
@@ -38,7 +38,8 @@ def hbnb_filters(the_id=None):
                            states=states,
                            amens=amens,
                            places=places,
-                           users=users)
+                           users=users,
+                           cache_id=uuid.uuid4())
 
 if __name__ == "__main__":
     """
