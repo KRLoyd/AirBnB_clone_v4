35831ef9faa20ee0f6e90868c401d40e7b0c1765
Kristen Loyd <loydkristen@gmail.com>
2017-10-02 17:52:48 +0000

    Updates to comply with semistandard style

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -31,54 +31,53 @@ $(function () {
 
   // Search when button is clicked
   $('button').click(function () {
-    console.log("dict: " + JSON.stringify(dict));
-    let amenityIdList = []
+    console.log('dict: ' + JSON.stringify(dict));
+    let amenityIdList = [];
     for (let i in dict) {
       amenityIdList.push(i);
     }
     let postDict = {};
-    postDict["amenities"] = amenityIdList;
+    postDict['amenities'] = amenityIdList;
     $.ajax({
       type: 'POST',
       url: 'http://0.0.0.0:5001/api/v1/places_search/',
       data: JSON.stringify(postDict),
       success: function (result) {
-        console.log("POST result: " + JSON.stringify(result));
+        console.log('POST result: ' + JSON.stringify(result));
         for (let i in result) {
           let structure = [
-'<article>',
-          '<div class="title">',
-          '<h2>' + result[i].name + '</h2>',
-          '<div class="price_by_night">' + '$' + result[i].price_by_night + '</div>',
-          '</div>',
-          '<div class="information">',
-          '<div class="max_guest">',
-          '<i class="fa fa-users fa-3x" aria-hidden="true"></i>',
-          '<br />',
-          result[i].max_guest + ' Guests',
-          '</div>',
-          '<div class="number_rooms">',
-          '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>',
-          '<br />',
-          result[i].number_rooms + ' Bedrooms',
-          '</div>',
-          '<div class="number_bathrooms">',
-          '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>',
-          '<br />',
-          result[i].number_bathrooms + ' Bathroom',
-          '</div>',
-          '</div>',
-          '<div class="description">',
-          result[i].description,
-          '</div>',
-          '</article>'
-        ];
+            '<article>',
+            '<div class="title">',
+            '<h2>' + result[i].name + '</h2>',
+            '<div class="price_by_night">' + '$' + result[i].price_by_night + '</div>',
+            '</div>',
+            '<div class="information">',
+            '<div class="max_guest">',
+            '<i class="fa fa-users fa-3x" aria-hidden="true"></i>',
+            '<br />',
+            result[i].max_guest + ' Guests',
+            '</div>',
+            '<div class="number_rooms">',
+            '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>',
+            '<br />',
+            result[i].number_rooms + ' Bedrooms',
+            '</div>',
+            '<div class="number_bathrooms">',
+            '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>',
+            '<br />',
+            result[i].number_bathrooms + ' Bathroom',
+            '</div>',
+            '</div>',
+            '<div class="description">',
+            result[i].description,
+            '</div>',
+            '</article>'
+          ];
           $(structure.join('')).appendTo('section.places');
         }
       },
       dataType: 'json',
       contentType: 'application/json'
     });
-
   });
 });
